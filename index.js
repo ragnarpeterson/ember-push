5ed6396b34d27d533ea3123baa3ba1ffec8e0eba
Jason Kriss <jasonkriss@gmail.com>
2015-05-20 12:43:57 -0700

    add pusher-js script tag

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-push'
+  name: 'ember-push',
+
+  contentFor: function(type) {
+    if (type === 'body') {
+      return '<script src="//js.pusher.com/2.2/pusher.min.js"></script>';
+    }
+  }
 };
